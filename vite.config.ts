0a7872dc503cccea7883bc57cbd1202d7a874532
José Battaglia <battagliajose@gmail.com>
2026-06-23 10:45:56 -0300

    Agregados Paths

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
       "@styles": path.resolve(__dirname, "./src/styles"),
       "@interfaces": path.resolve(__dirname, "./src/interfaces"),
       "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@API": path.resolve(__dirname, "./src/API"),
     },
   },
 });
