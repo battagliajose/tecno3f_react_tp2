33b979609d056ef477825479da94eb6d06d14938
José Battaglia <battagliajose@gmail.com>
2026-06-24 08:47:39 -0300

    Ajustes de Home, CSS y se agrega imagen

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router";
-import CustomFooter from "@components/CustomFooter";
-import CustomHeader from "@components/CustomHeader";
+import "@styles/home.css";
+import HomeImage from "@assets/Home.png";
 
 export default function Home() {
   return (
-    <>
-      <CustomHeader />
+    <div className="home-container">
       <h1>Bienvenido a la página de inicio</h1>
       <p>Explora el catálogo de personajes de Rick and Morty.</p>
+      <img className="home-image" src={HomeImage} alt="Home" />
       <Link to="/catalogo">Ir al catálogo</Link>
-      <CustomFooter />
-    </>
+    </div>
   );
 }
