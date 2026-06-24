4fe385537cf73dc9f1ae93af9e767a65d110b254
José Battaglia <battagliajose@gmail.com>
2026-06-24 11:02:56 -0300

    Movido contador de Favoritos a NavBar

--- src/components/CustomNavBar.tsx
+++ src/components/CustomNavBar.tsx
@@ -1,18 +1,27 @@
 import { Link } from "react-router";
 import "@styles/nav-bar.css";
+import { useContext } from "react";
+import { favoriteContext } from "@/context/favoriteContext";
 
 export default function CustomNavBar() {
+  const { favorites } = useContext(favoriteContext);
+
   return (
     <nav className="custom-navbar">
-      <Link className="custom-navbar-link" to="/">
-        Inicio
-      </Link>
-      <Link className="custom-navbar-link" to="/catalogo">
-        Catálogo
-      </Link>
-      <Link className="custom-navbar-link" to="/contacto">
-        Contacto
-      </Link>
+      <div className="custom-navbar-links">
+        <Link className="custom-navbar-link" to="/">
+          Inicio
+        </Link>
+        <Link className="custom-navbar-link" to="/catalogo">
+          Catálogo
+        </Link>
+        <Link className="custom-navbar-link" to="/contacto">
+          Contacto
+        </Link>
+      </div>
+      <span className="custom-navbar-favorites">
+        Favoritos: {favorites.length}
+      </span>
     </nav>
   );
 }
--- src/pages/catalogo/Catalogo.tsx
+++ src/pages/catalogo/Catalogo.tsx
@@ -1,13 +1,8 @@
 import MainContent from "./componentes/MainContent";
-import { useContext } from "react";
-import { favoriteContext } from "@/context/favoriteContext";
 
 export default function Catalogo() {
-  const { favorites } = useContext(favoriteContext);
-
   return (
     <>
-      <h1>Favoritos: {favorites.length}</h1>
       <MainContent />
     </>
   );
