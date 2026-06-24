50ce55bcbff5f081d582d0a611f359862df6c683
José Battaglia <battagliajose@gmail.com>
2026-06-24 08:44:02 -0300

    Formulario de Contacto con react-hook-fomr

--- src/pages/contacto/Contacto.tsx
+++ src/pages/contacto/Contacto.tsx
@@ -1,13 +1,64 @@
+import { useForm } from "react-hook-form";
+import "@styles/contacto.css";
+
 export default function Contacto() {
+  interface ContactoProps {
+    nombre: string;
+    email: string;
+    mensaje: string;
+  }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<ContactoProps>();
+
   return (
-    <>
+    <div className="contacto-container">
       <h1>Contacto</h1>
       <p>Si tienes alguna pregunta o comentario, no dudes en contactarnos.</p>
-      <ul>
-        <li>Email: contacto@rickandmorty.com</li>
-        <li>Teléfono: +1 234 567 890</li>
-        <li>Dirección: Calle Ficticia 123, Ciudad Imaginaria</li>
-      </ul>
-    </>
+      <form
+        className="contacto-form"
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+          reset();
+        })}
+      >
+        <div className="contacto-form-group">
+          <label htmlFor="nombre">Nombre:</label>
+          <input
+            id="nombre"
+            {...register("nombre", { required: "El nombre es obligatorio" })}
+          />
+          {errors.nombre && (
+            <span className="spanError">{errors.nombre.message}</span>
+          )}
+        </div>
+        <div className="contacto-form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            type="email"
+            {...register("email", { required: "El email es obligatorio" })}
+          />
+          {errors.email && (
+            <span className="spanError">{errors.email.message}</span>
+          )}
+        </div>
+        <div className="contacto-form-group">
+          <label htmlFor="mensaje">Mensaje:</label>
+          <textarea
+            id="mensaje"
+            {...register("mensaje", { required: "El mensaje es obligatorio" })}
+          />
+          {errors.mensaje && (
+            <span className="spanError">{errors.mensaje.message}</span>
+          )}
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
   );
 }
